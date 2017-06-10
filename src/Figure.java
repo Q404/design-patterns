b4c7d290d0a97c1afa9a024acd9a35e7a4fc77a9
Anton Borvanov <mr.a404@gmail.com>
2017-06-10 15:30:32 +0300

    Interfaces added

---
+++ src/Figure.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+/**
+ * Created by mrq40 on 08/06/17.
+ */
+public interface Figure {
+    public void draw(Graphics g);
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by mrq40 on 08/06/17.
  */
 public class Runner {
     public static void main(String[] args) {
-        
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ignored) {}
+
+        /* Main window creating */
+
+        JFrame window = new JFrame("33 on fire");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(500, 400);
+
+        /* Upper tab menu */
+
+        JPanel tabBar = new JPanel(new BorderLayout());
+        JComboBox<Picture> pictureJComboBox = new JComboBox<>();
+        JComboBox<PictureColorDecorator> pictureColorDecoratorJComboBox = new JComboBox<>();
+        tabBar.add(pictureJComboBox);
+        tabBar.add(pictureColorDecoratorJComboBox);
     }
 }
